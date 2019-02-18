9d425a928c9d0e4e10d69d3356baef008b4abdea
Anders Hesselbom <anders.hesselbom@nethouse.se>
2019-02-18 17:01:03 +0100

    Early version of map view.

--- MemVisualizer/MemoryVisualizer/MainWindow.cs
+++ MemVisualizer/MemoryVisualizer/MainWindow.cs
@@ -7,213 +7,213 @@ using MemoryVisualizer.Renderer;
 
 namespace MemoryVisualizer
 {
-	public partial class MainWindow : Form
-	{
-		private readonly ScreenPainter _screenPainter = new ScreenPainter();
-		private IScreenRenderer _screenRenderer;
-		private Memory Memory { get; set; }
-		private Address DisplayPointer { get; } = new Address(0);
-		private DisplayMode DisplayMode { get; set; }
-		private ScreenCharacterMap Characters { get; } = new ScreenCharacterMap();
-		private int StepSize { get; set; }
-		private MemOverview MemOverview { get; set; }
-		private Address DisassemblyStartAddress { get; } = new Address(0);
-		private int LastPerformedDisassemblyStepSize { get; set; }
-		private Stack<int> DisassemblyStepSize { get; } = new Stack<int>();
+    public partial class MainWindow : Form
+    {
+        private readonly ScreenPainter _screenPainter = new ScreenPainter();
+        private IScreenRenderer _screenRenderer;
+        private Memory Memory { get; set; }
+        private Address DisplayPointer { get; } = new Address(0);
+        private DisplayMode DisplayMode { get; set; }
+        private ScreenCharacterMap Characters { get; } = new ScreenCharacterMap();
+        private int StepSize { get; set; }
+        private MemOverview MemOverview { get; set; }
+        private Address DisassemblyStartAddress { get; } = new Address(0);
+        private int LastPerformedDisassemblyStepSize { get; set; }
+        private Stack<int> DisassemblyStepSize { get; } = new Stack<int>();
 
-		public MainWindow()
-		{
-			InitializeComponent();
-			_screenRenderer = new HexRawScreenRenderer(ScreenCharacterMap.Rows, Characters);
-		}
+        public MainWindow()
+        {
+            InitializeComponent();
+            _screenRenderer = new HexRawScreenRenderer(ScreenCharacterMap.Rows, Characters);
+        }
 
-		private void MainWindow_Resize(object sender, EventArgs e)
-		{
-			_screenPainter.RecalcGridFontSize = true;
-			if (Memory != null)
-				MemOverview = MemOverview.Create(Memory, Bounds.Height > 10 ? Bounds.Height - 5 : 10);
-			Invalidate();
-		}
-		private void MainWindow_Paint(object sender, PaintEventArgs e)
-		{
-			_screenPainter.Paint(e.Graphics, Memory, ClientSize, menuStrip1.Height, ScreenCharacterMap.Rows, ScreenCharacterMap.Columns, Characters);
-			MemOverview?.Draw(e.Graphics, DisplayPointer.Value);
-		}
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            _screenPainter.RecalcGridFontSize = true;
+            if (Memory != null)
+                MemOverview = MemOverview.Create(Memory);
+            Invalidate();
+        }
+        private void MainWindow_Paint(object sender, PaintEventArgs e)
+        {
+            _screenPainter.Paint(e.Graphics, Memory, ClientSize, menuStrip1.Height, ScreenCharacterMap.Rows, ScreenCharacterMap.Columns, Characters);
+            MemOverview?.Draw(e.Graphics, DisplayPointer.Value, 10, menuStrip1.Height + 10);
+        }
 
-		private void quitToolStripMenuItem_Click(object sender, EventArgs e) =>
-			Close();
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e) =>
+            Close();
 
-		private void openToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			using (var x = new OpenFileDialog())
-			{
-				x.Title = @"Select a C64 program to open";
-				x.Filter = @"Programs (*.prg)|*.prg|All files (*.*)|*.*";
-				if (x.ShowDialog(this) == DialogResult.OK)
-					LoadFile(x.FileName);
-			}
-		}
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var x = new OpenFileDialog())
+            {
+                x.Title = @"Select a C64 program to open";
+                x.Filter = @"Programs (*.prg)|*.prg|All files (*.*)|*.*";
+                if (x.ShowDialog(this) == DialogResult.OK)
+                    LoadFile(x.FileName);
+            }
+        }
 
-		private void LoadFile(string filename)
-		{
-			try
-			{
-				var temp = new Memory();
-				temp.Load(filename, out var start, out _);
-				Text = $@"C64 Memory Visualizer - {filename}";
-				Memory = temp;
-				DisplayPointer.FromInt(start.Value);
-				DisassemblyStartAddress.FromInt(start.Value);
-				DisplayMode = DisplayMode.HexRaw;
-				if (Memory != null)
-					MemOverview = MemOverview.Create(Memory, Bounds.Height > 10 ? Bounds.Height - 5 : 10);
-				RenderScreen();
-			}
-			catch (Exception)
-			{
-				MessageBox.Show($@"The file ""{filename}"" could not be loaded.", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
-		}
+        private void LoadFile(string filename)
+        {
+            try
+            {
+                var temp = new Memory();
+                temp.Load(filename, out var start, out _);
+                Text = $@"C64 Memory Visualizer - {filename}";
+                Memory = temp;
+                DisplayPointer.FromInt(start.Value);
+                DisassemblyStartAddress.FromInt(start.Value);
+                DisplayMode = DisplayMode.HexRaw;
+                if (Memory != null)
+                    MemOverview = MemOverview.Create(Memory);
+                RenderScreen();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($@"The file ""{filename}"" could not be loaded.", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
-		private void MainWindow_DragDrop(object sender, DragEventArgs e)
-		{
-			var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-			if (files.Length > 0)
-				LoadFile(files[0]);
-		}
+        private void MainWindow_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+                LoadFile(files[0]);
+        }
 
-		private void MainWindow_DragOver(object sender, DragEventArgs e)
-		{
-			if (e.Data.GetDataPresent(DataFormats.FileDrop))
-				e.Effect = DragDropEffects.Copy;
-		}
+        private void MainWindow_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
 
-		private void RenderScreen()
-		{
-			if (Memory == null)
-				return;
-			Characters.Clear();
-			var displayPointer = (int)DisplayPointer.Value;
-			switch (DisplayMode)
-			{
-				case DisplayMode.HexRaw:
-				case DisplayMode.DecRaw:
-					StepSize = _screenRenderer.Render(ref displayPointer, Memory);
-					break;
-				case DisplayMode.Disassembly:
-					LastPerformedDisassemblyStepSize = StepSize = _screenRenderer.Render(ref displayPointer, Memory);
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
-			Invalidate();
-		}
+        private void RenderScreen()
+        {
+            if (Memory == null)
+                return;
+            Characters.Clear();
+            var displayPointer = (int)DisplayPointer.Value;
+            switch (DisplayMode)
+            {
+                case DisplayMode.HexRaw:
+                case DisplayMode.DecRaw:
+                    StepSize = _screenRenderer.Render(ref displayPointer, Memory);
+                    break;
+                case DisplayMode.Disassembly:
+                    LastPerformedDisassemblyStepSize = StepSize = _screenRenderer.Render(ref displayPointer, Memory);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            Invalidate();
+        }
 
-		private void MainWindow_KeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.KeyCode == Keys.PageUp)
-				previousPageToolStripMenuItem_Click(sender, new EventArgs());
-			else if (e.KeyCode == Keys.PageDown)
-				nextPageToolStripMenuItem_Click(sender, new EventArgs());
-		}
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.PageUp)
+                previousPageToolStripMenuItem_Click(sender, new EventArgs());
+            else if (e.KeyCode == Keys.PageDown)
+                nextPageToolStripMenuItem_Click(sender, new EventArgs());
+        }
 
-		private void previousPageToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			switch (DisplayMode)
-			{
-				case DisplayMode.Disassembly:
-					if (DisassemblyStepSize.Count > 0)
-					{
-						var step = DisassemblyStepSize.Pop();
-						if (DisplayPointer.CanDec(step))
-							DisplayPointer.Dec(step);
-					}
-					break;
-				default:
-					if (DisplayPointer.CanDec(StepSize))
-						DisplayPointer.Dec(StepSize);
-					else
-						DisplayPointer.FromInt(0);
-					break;
-			}
-			RenderScreen();
-		}
+        private void previousPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            switch (DisplayMode)
+            {
+                case DisplayMode.Disassembly:
+                    if (DisassemblyStepSize.Count > 0)
+                    {
+                        var step = DisassemblyStepSize.Pop();
+                        if (DisplayPointer.CanDec(step))
+                            DisplayPointer.Dec(step);
+                    }
+                    break;
+                default:
+                    if (DisplayPointer.CanDec(StepSize))
+                        DisplayPointer.Dec(StepSize);
+                    else
+                        DisplayPointer.FromInt(0);
+                    break;
+            }
+            RenderScreen();
+        }
 
-		private void nextPageToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			switch (DisplayMode)
-			{
-				case DisplayMode.Disassembly:
-					if (LastPerformedDisassemblyStepSize > 0)
-					{
-						DisassemblyStepSize.Push(LastPerformedDisassemblyStepSize);
-						DisplayPointer.Inc(LastPerformedDisassemblyStepSize);
-					}
-					break;
-				default:
-					if (DisplayPointer.CanInc(StepSize))
-						DisplayPointer.Inc(StepSize);
-					break;
-			}
-			RenderScreen();
-		}
+        private void nextPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            switch (DisplayMode)
+            {
+                case DisplayMode.Disassembly:
+                    if (LastPerformedDisassemblyStepSize > 0)
+                    {
+                        DisassemblyStepSize.Push(LastPerformedDisassemblyStepSize);
+                        DisplayPointer.Inc(LastPerformedDisassemblyStepSize);
+                    }
+                    break;
+                default:
+                    if (DisplayPointer.CanInc(StepSize))
+                        DisplayPointer.Inc(StepSize);
+                    break;
+            }
+            RenderScreen();
+        }
 
-		private void rawHexToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			foreach (ToolStripMenuItem item in viewToolStripMenuItem.DropDownItems)
-				item.Checked = false;
-			rawHexToolStripMenuItem.Checked = true;
-			DisplayMode = DisplayMode.HexRaw;
-			_screenRenderer = new HexRawScreenRenderer(ScreenCharacterMap.Rows, Characters);
-			RenderScreen();
-		}
+        private void rawHexToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ToolStripMenuItem item in viewToolStripMenuItem.DropDownItems)
+                item.Checked = false;
+            rawHexToolStripMenuItem.Checked = true;
+            DisplayMode = DisplayMode.HexRaw;
+            _screenRenderer = new HexRawScreenRenderer(ScreenCharacterMap.Rows, Characters);
+            RenderScreen();
+        }
 
-		private void rawDecToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			foreach (ToolStripMenuItem item in viewToolStripMenuItem.DropDownItems)
-				item.Checked = false;
-			rawDecToolStripMenuItem.Checked = true;
-			DisplayMode = DisplayMode.DecRaw;
-			_screenRenderer = new DecRawScreenRenderer(ScreenCharacterMap.Rows, Characters);
-			RenderScreen();
-		}
+        private void rawDecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ToolStripMenuItem item in viewToolStripMenuItem.DropDownItems)
+                item.Checked = false;
+            rawDecToolStripMenuItem.Checked = true;
+            DisplayMode = DisplayMode.DecRaw;
+            _screenRenderer = new DecRawScreenRenderer(ScreenCharacterMap.Rows, Characters);
+            RenderScreen();
+        }
 
-		private void disassemblyToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			if (!SetDisassemblyStartAddress())
-				return;
-			foreach (ToolStripMenuItem item in viewToolStripMenuItem.DropDownItems)
-				item.Checked = false;
-			disassemblyToolStripMenuItem.Checked = true;
-			DisplayMode = DisplayMode.Disassembly;
-			_screenRenderer = new DisassemblyScreenRenderer(ScreenCharacterMap.Rows, Characters);
-			RenderScreen();
-		}
+        private void disassemblyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!SetDisassemblyStartAddress())
+                return;
+            foreach (ToolStripMenuItem item in viewToolStripMenuItem.DropDownItems)
+                item.Checked = false;
+            disassemblyToolStripMenuItem.Checked = true;
+            DisplayMode = DisplayMode.Disassembly;
+            _screenRenderer = new DisassemblyScreenRenderer(ScreenCharacterMap.Rows, Characters);
+            RenderScreen();
+        }
 
-		private void editToolStripMenuItem_DropDownOpening(object sender, EventArgs e) =>
-			setDisassemblyStartAddressToolStripMenuItem.Enabled = Memory != null && DisplayMode == DisplayMode.Disassembly;
+        private void editToolStripMenuItem_DropDownOpening(object sender, EventArgs e) =>
+            setDisassemblyStartAddressToolStripMenuItem.Enabled = Memory != null && DisplayMode == DisplayMode.Disassembly;
 
-		private void setDisassemblyStartAddressToolStripMenuItem_Click(object sender, EventArgs e) =>
-			SetDisassemblyStartAddress();
+        private void setDisassemblyStartAddressToolStripMenuItem_Click(object sender, EventArgs e) =>
+            SetDisassemblyStartAddress();
 
-		private bool SetDisassemblyStartAddress()
-		{
-			using (var x = new DialogDisassemblyStartAddress())
-			{
-				x.StartAddress = DisassemblyStartAddress.Value;
-				if (x.ShowDialog(this) != DialogResult.OK)
-					return false;
-				DisassemblyStartAddress.FromInt(x.StartAddress);
-				DisassemblyStepSize.Clear();
-				DisplayPointer.FromInt(x.StartAddress);
-				RenderScreen();
-				return true;
-			}
-		}
-		private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        private bool SetDisassemblyStartAddress()
+        {
+            using (var x = new DialogDisassemblyStartAddress())
+            {
+                x.StartAddress = DisassemblyStartAddress.Value;
+                if (x.ShowDialog(this) != DialogResult.OK)
+                    return false;
+                DisassemblyStartAddress.FromInt(x.StartAddress);
+                DisassemblyStepSize.Clear();
+                DisplayPointer.FromInt(x.StartAddress);
+                RenderScreen();
+                return true;
+            }
+        }
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DisplayPointer.FromInt(DisplayMode == DisplayMode.Disassembly ? DisassemblyStartAddress.Value : 0);
             RenderScreen();
         }
-	}
+    }
 }
--- MemVisualizer/MemoryVisualizer/MemOverview.cs
+++ MemVisualizer/MemoryVisualizer/MemOverview.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
 using C64MemoryModel.Mem;
 
 namespace MemoryVisualizer
@@ -7,25 +9,71 @@ namespace MemoryVisualizer
     public class MemOverview
     {
         private int[] Data { get; set; }
+        private Bitmap Bitmap { get; set; }
+        private static int _height = 512;
+        private static int _bytesPerPixel = 128;
+        private static int _width = 16;
 
-        public static MemOverview Create(Memory memory, int height)
+        public static MemOverview Create(Memory memory)
         {
             //Create the overview.
             var x = new MemOverview {
-                Data = new int[height]
+                Data = new int[_height]
             };
-            //Initialize
-            var stepFactor = 1;
-            if (height < ushort.MaxValue)
-                stepFactor = (int)Math.Ceiling((double)ushort.MaxValue/height);
-            var maxValue = 255*stepFactor;
-            //TODO!
+            var memIndex = 0;
+            for (var yIndex = 0; yIndex < _height; yIndex++)
+            {
+                var byteSum = 0;
+                for (var y = 0; y < _bytesPerPixel; y++)
+                {
+                    byteSum += memory.GetByte((ushort)memIndex);
+                    memIndex++;
+                }
+                x.Data[yIndex] = byteSum;
+            }
+            x.NormalizeData();
+            x.CreateBitmap();
             return x;
         }
 
-        public void Draw(Graphics g, int displayPointer)
+        private void NormalizeData()
         {
+            var highest = (double)Data.Concat(new[] {0}).Max();
+            if (highest <= 0)
+                return;
+            for(var i = 0; i < Data.Length; i++)
+            {
+                if (Data[i] <= 0)
+                    continue;
+                var percent = Data[i] / highest;
+                var newValue = Math.Round(255 * percent);
+                Data[i] = (int)newValue;
+                if (Data[i] > 255)
+                    Data[i] = 255;
+                else if (Data[i] < 0)
+                    Data[i] = 0;
+            }
+        }
 
+        private void CreateBitmap()
+        {
+            Bitmap = new Bitmap(_width, _height);
+            using (var g = Graphics.FromImage(Bitmap))
+            {
+                g.SmoothingMode = SmoothingMode.None;
+                for (var i = 0; i < Data.Length; i++)
+                {
+                    using (var p = new Pen(Color.FromArgb(Data[i], Data[i], Data[i])))
+                        g.DrawLine(p, 0, i, _width, i);
+                }
+            }
+        }
+
+        public void Draw(Graphics g, int displayPointer, int x, int y)
+        {
+            g.DrawImage(Bitmap, x, y);
+            var ypos = y + (displayPointer / _bytesPerPixel);
+            g.DrawLine(Pens.Red, x + 7, ypos, x + 20, ypos);
         }
     }
 }
