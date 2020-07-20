5dbd88b8865b3c74a8816e59f0d35d2fde6d7a48
Anders-H <anders@winsoft.se>
2020-07-20 22:39:30 +0200

    List view columns.

--- Monitor/MainWindow.Designer.cs
+++ Monitor/MainWindow.Designer.cs
@@ -32,7 +32,14 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lv = new System.Windows.Forms.ListView();
+            this.colOperation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colMode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colAddressHex = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colAddressDec = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colParam1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colParam2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colMachineCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -60,22 +67,67 @@
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // listView1
+            // lv
             // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(0, 49);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(800, 379);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.lv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colAddressHex,
+            this.colAddressDec,
+            this.colOperation,
+            this.colMode,
+            this.colParam1,
+            this.colParam2,
+            this.colMachineCode});
+            this.lv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lv.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(0, 49);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(800, 379);
+            this.lv.TabIndex = 3;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.View = System.Windows.Forms.View.Details;
+            // 
+            // colOperation
+            // 
+            this.colOperation.Text = "Operation";
+            this.colOperation.Width = 90;
+            // 
+            // colMode
+            // 
+            this.colMode.Text = "Mode";
+            this.colMode.Width = 90;
+            // 
+            // colAddressHex
+            // 
+            this.colAddressHex.Text = "Address (HEX)";
+            this.colAddressHex.Width = 90;
+            // 
+            // colAddressDec
+            // 
+            this.colAddressDec.Text = "Address (DEC)";
+            this.colAddressDec.Width = 90;
+            // 
+            // colParam1
+            // 
+            this.colParam1.Text = "Parameter 1";
+            this.colParam1.Width = 90;
+            // 
+            // colParam2
+            // 
+            this.colParam2.Text = "Parameter 2";
+            this.colParam2.Width = 90;
+            // 
+            // colMachineCode
+            // 
+            this.colMachineCode.Text = "Machine code";
+            this.colMachineCode.Width = 150;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lv);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
@@ -93,7 +145,14 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lv;
+        private System.Windows.Forms.ColumnHeader colAddressHex;
+        private System.Windows.Forms.ColumnHeader colAddressDec;
+        private System.Windows.Forms.ColumnHeader colOperation;
+        private System.Windows.Forms.ColumnHeader colMode;
+        private System.Windows.Forms.ColumnHeader colParam1;
+        private System.Windows.Forms.ColumnHeader colParam2;
+        private System.Windows.Forms.ColumnHeader colMachineCode;
     }
 }
 
